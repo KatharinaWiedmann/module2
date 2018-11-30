a45355cc0cf4abd2ad5789746fe958bca7548bc2
Katharina Wiedmann <katie.wiedmann@gmx.de>
2018-11-30 03:00:51 +0000

    Homework day02 in class

---
+++ ch02/ch2_katie.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 29 11:47:57 2018
+
+@author: 612383447
+"""
+A = 10/3
+print(A)
+
+B = 10%3
+print (B)
+
+a = 1
+a = a+1
+print (a)
+
+b = 'hello'
+print (b)
+
+c = b.title()
+print (b)
+print (c)
+
+d = "hello"
+e = d.title()
+print(d)
+print (e)
+name = "Dave"
+f = "Hello {0}!".format(name)
+print (f)
+name = "Sarah"
+print (f)
+print (f*5)
