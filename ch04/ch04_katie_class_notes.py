f2efab5560b4f1d1e9ed239e07387d13c8f6553d
Katharina Wiedmann <katie.wiedmann@gmx.de>
2018-12-05 07:08:47 +0000

    Committing class notes day 4.

---
+++ ch04/ch04_katie_class_notes.py
@@ -0,0 +1,104 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec  4 11:47:39 2018
+
+@author: 612383447
+"""
+#
+#import random 
+#import datetime
+#import time
+#
+#def luckyNumberRandom():
+#    name = input('Please type your name here: ')
+#    print('Hello ' + name.upper())
+#    number = int(input('Please give me a number: '))
+#    
+#    print ('Your lucky number is: ' +str(random.randint(1, number)))
+#    
+#
+#
+#startTime = time.time()
+#
+#print ('date and time', datetime.datetime.now())
+#print ()
+#print ('current time', datetime.datetime.now().time())
+#
+#luckyNumberRandom()
+#
+#processTime = time.time() - startTime
+#
+#print()
+#print('programme running time: ', round(processTime,2),'second')  
+#
+#age = 15 
+#isaTeen = age >= 13 and age <= 19
+#
+#print(isaTeen)
+#
+#
+#number = input("Choose a number between 1 and 10: ")
+#number = int(number)
+#
+#if number > 10:
+#    print("Too high!")
+#    
+#elif number <= 0:
+#    print("Too low!")
+#
+#else:
+#    print("Thank you, I will note {} as your favourite number.".format(number))
+#
+#  
+
+#age = input("How old are you? ")
+#age = int(age)
+#
+#if age < 13:
+#    print("child")
+#    
+#elif age < 18:
+#    print("teen")
+#
+#elif age < 65:
+#    print("adult")
+#    
+#else: 
+#    print("pensioner")
+#    
+#
+#support = input("Which football club do you support? FC Bayern Munich or Dortmund? ")
+#
+#if (support == 'FC Bayern Munich'):
+#    print("Well done, good choice!")
+#
+#else:
+#    print ("Really, you support {} ?! WHYYYY?".format(support))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
