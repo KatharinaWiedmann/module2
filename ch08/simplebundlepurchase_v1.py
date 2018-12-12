1c343dad4210570a29f410284791bbe1ce63ba1b
Katharina Wiedmann <katie.wiedmann@gmx.de>
2018-12-12 16:18:16 +0000

    Ask PW three times before declining access.

--- ch08/simplebundlepurchase_v1.py
+++ ch08/simplebundlepurchase_v1.py
@@ -6,7 +6,7 @@ Created on Wed Dec 12 04:50:08 2018
 """
 
 def DataBundlePurchase(truePasscode, balance):
-    if passwordCheck(truePasscode):
+    if passwordCheck(truePasscode) or passwordDoubleCheck(truePasscode) or passwordTrippleCheck(truePasscode):
         task = checkTask()
         if task == '1':
             displayBalance(balance)
@@ -21,23 +21,42 @@ def DataBundlePurchase(truePasscode, balance):
                       print("You just purchased data worth {}" .format(amount))
                       newBalance = calculateNewBalance(amount, balance)
                       print("Your new balance is {}.".format(newBalance))
+                      return newBalance
                    elif amount < multiple_of_five and (amount%5 != 0):
-                        return "It is only possible to purchase a multiple of five."
+                        print("It is only possible to purchase a multiple of five.")
+                        return 'amount not a multiple of five' 
                    elif amount > multiple_of_five and (amount%5 == 0):
-                       return 'Transaction failed due to insufficient balance'     
+                       print('Transaction failed due to insufficient balance') 
+                       return 'insufficient balance'
                else:
-                   return ('Your balance of {} is not sufficient.'.format(balance))
+                   print('Your balance of {} is not sufficient.'.format(balance))
+                   return 'insufficient balance'
            else:
-               return("The phone numbers don't match - unfortunately we cannot proceed here.")
+               print("The phone numbers don't match - unfortunately we cannot proceed here.")
+               return 'phone numbers don\'t match'
 #           
            
     else:
-        return 'Wrong password'
-    
-    #return ('not yet created')
-    
+        print ('Wrong password - Access declined')
+        return 'Access declined'
+        
+        
 def passwordCheck(truePasscode):
     attempt = input('Please enter your password ') 
+    if attempt == truePasscode:
+        return True
+    else:
+        return False
+    
+def passwordDoubleCheck(truePasscode):
+    attempt = input('Please try again ') 
+    if attempt == truePasscode:
+        return True
+    else:
+        return False
+
+def passwordTrippleCheck(truePasscode):
+    attempt = input('Last attempt, please try again ') 
     if attempt == truePasscode:
         return True
     else:
@@ -67,7 +86,7 @@ def checkBalance(balance):
         return False
    
 def multipleOfFive(balance):
-    remainder = (int(balance)%5) 
+    remainder = balance%5 
     multiple_of_five = int(balance) - int(remainder)
     return multiple_of_five
     print(multiple_of_five)   
@@ -77,5 +96,5 @@ def topup(multiple_of_five):
     return amount 
  
 def calculateNewBalance(amount, balance):
-    newBalance = balance - amount
+    newBalance = round(balance - amount,3)
     return newBalance
--- ch08/test_databundlepurchase_v1.py
+++ ch08/test_databundlepurchase_v1.py
@@ -21,7 +21,7 @@ print('-'*50, '\n')
 
 
 
-print('TEST EXAMPLE 2')
+print('TEST EXAMPLE 3')
 result = DataBundlePurchase('3456', 17.55)
 print('----\nRESULT:', result)
 print('-'*50, '\n')
